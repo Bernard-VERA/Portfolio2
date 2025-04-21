8e521817d9e23864b6290ed2a3b59ca2831fcbce
Bernard-VERA <bvmars@hotmail.com>
2025-04-21 14:13:45 +0200

    Modification du texte A propos + 100dvh sur Home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,13 +6,13 @@ function About() {
       <h1>À propos</h1>
       <div className="about-content">
         <p>
-          Passionné depuis toujours par l'informatique, j'ai décidé aujourd'hui d'en faire mon métier, 
-          en devenant développeur full-stack. Les possibilités infinies de création, les défis techniques 
-          et la satisfaction de voir mes projets prendre vie sont autant de raisons qui m'ont poussé à me reconvertir.
+        Passionné d’informatique depuis toujours, j’ai choisi d’en faire mon métier en me spécialisant dans le développement full-stack.
         </p>
         <p>
-          Mon cursus de formation chez OpenClassrooms, m'a permis d'améliorer l'expérience que j'ai acquise en autodidacte. 
-          Mon but est de devenir un développeur web polyvalent, capable de concevoir des sites intuitifs et performants.
+        Soucieux d’enrichir mon parcours autodidacte, j’ai suivi une formation diplômante chez OpenClassrooms. Cela m’a permis d’acquérir des compétences solides, pour concevoir des expériences web intuitives et performantes.
+        </p>
+        <p>
+        Curieux et toujours en quête d’amélioration, je mets mon expertise au service de projets innovants, alliant technicité et créativité, pour répondre aux besoins des utilisateurs. 
         </p>
       </div>
     </div>
