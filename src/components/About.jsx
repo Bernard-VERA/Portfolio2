b1848673cc057abe887f5f4edffe440ebd68cef5
Bernard-VERA <bvmars@hotmail.com>
2025-04-24 00:40:02 +0200

    Ajout photo

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,14 @@ function About() {
     <div className="about">
       <h1>À propos</h1>
       <div className="about-content">
+        <div>
         <p>
-        Passionné d’informatique depuis toujours, j’ai choisi d’en faire mon métier en me spécialisant dans le développement full-stack.
-        </p>
-        <p>
-        Soucieux d’enrichir mon parcours autodidacte, j’ai suivi une formation diplômante chez OpenClassrooms. Cela m’a permis d’acquérir des compétences solides, pour concevoir des expériences web intuitives et performantes.
-        </p>
-        <p>
+        Passionné d’informatique depuis toujours, j’ai choisi d’en faire mon métier en me spécialisant dans le développement full-stack.<br/>
+        Soucieux d’enrichir mon parcours autodidacte, j’ai suivi une formation diplômante chez OpenClassrooms. Cela m’a permis d’acquérir des compétences solides, pour concevoir des expériences web intuitives et performantes.<br/>
         Curieux et toujours en quête d’amélioration, je mets mon expertise au service de projets innovants, alliant technicité et créativité, pour répondre aux besoins des utilisateurs. 
         </p>
+        </div>
+        <img src="/images/portrait.jpg" alt="Avatar" className="avatar" />
       </div>
     </div>
   );
