ce79df343c08160f724c436ebbe4d7fa53eaccb0
Bernard-VERA <bvmars@hotmail.com>
2025-04-29 23:40:51 +0200

    Ajustement css section softskills

--- src/components/Softskills.jsx
+++ src/components/Softskills.jsx
@@ -4,15 +4,13 @@ function Softskills() {
     return (
       <div className="softskills">
         <h2>Mes Soft skills</h2>
-        <div className="softskills-container">
-          
+        <div className="softskills-content"> 
           <div className='tech-skills'>
             <h3>Soft skills techniques</h3>
           </div>
           <div className='life-skills'>
             <h3>Mon savoir-être</h3>
           </div>
-          
         </div>
       </div>
     );
